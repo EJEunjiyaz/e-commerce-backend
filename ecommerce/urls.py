fae145097996b1d8f1dba593fa1f9bc859f37fea
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2021-04-28 06:30:25 +0700

    Merge branch 'rest_framework'
    # Conflicts:
    #	ecommerce/settings.py
    #	ecommerce/urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('website.urls')),
-    path('', include('webapp.urls')),
+    path('', include('website.urls')),
+    path('api/', include('webapp.urls')),
 ]
