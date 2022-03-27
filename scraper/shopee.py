baadc895bce667593a3501aad642357347b56975
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2022-03-28 06:05:11 +0700

    Variations into dict

--- scraper/shopee.py
+++ scraper/shopee.py
@@ -8,20 +8,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
-driver.get("https://shopee.co.th/VICTOR-%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B9%81%E0%B8%9A%E0%B8%94%E0%B8%A1%E0%B8%B4%E0"
-           "%B8%99%E0%B8%95%E0%B8%B1%E0%B8%99-Graphite-%E0%B8%84%E0%B8%B8%E0%B8%93%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B8"
-           "%AA%E0%B8%B9%E0%B8%87%E0%B8%AA%E0%B8%B3%E0%B8%AB%E0%B8%A3%E0%B8%B1%E0%B8%9A%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0"
-           "%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88%E0%B8%96%E0%B8%B6%E0%B8%87%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B9%82%E0%B8%9B"
-           "%E0%B8%A3-%E0%B8%9E%E0%B8%A3%E0%B9%89%E0%B8%AD%E0%B8%A1%E0%B8%82%E0%B8%B6%E0%B9%89%E0%B8%99%E0%B9%80%E0"
-           "%B8%AD%E0%B9%87%E0%B8%99-24%E0%B8%9B%E0%B8%AD%E0%B8%99%E0%B8%94%E0%B9%8C-%E0%B8%9F%E0%B8%A3%E0%B8%B5%E0"
-           "%B8%8B%E0%B8%AD%E0%B8%87-i.80229818.1977213011?sp_atk=41cbbfb4-e9ef-42f4-b9c5-fef8debbbb89")
+driver.get("https://shopee.co.th/Apple-iPhone-13-%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2%E0%B8%88%E0%B8%AD-6.1-%E0%B8%99"
+           "%E0%B8%B4%E0%B9%89%E0%B8%A7-i.287137993.10951724608?sp_atk=f674b50f-08d3-47a2-b7da-ac0606b4f4f7")
+print("Load Shopee item successful.")
 
 """
 Click Thai language selected at welcome.
 """
+print("Start to select language dialog.")
 language_dialog = WebDriverWait(driver, timeout=5).until(
     lambda d: d.find_element(By.CLASS_NAME, "shopee-button-outline.shopee-button-outline--primary-reverse "))
 language_dialog.click()
+print("Click language dialog successful.")
 
 """
 Collect product name 
@@ -32,22 +30,39 @@ print(product_name.text)
 """
 Search for available variations.
 """
-available_variations = WebDriverWait(driver, timeout=5).until(
-    lambda d: d.find_elements(By.XPATH, '//button[@class="product-variation" and @aria-disabled="false"]'))
-for variation in available_variations:
-    print(variation.text)
-    variation.click()
-    sleep(1)
+options = WebDriverWait(driver, timeout=5).until(lambda d: d.find_elements(By.XPATH, '//label[@class="koZBMj"]'))
+default_options = ["ช้อปเพิ่มคุ้มกว่า", "การจัดส่ง"]
+"""available_options for user to custom their own variations."""
+available_options = []
 
-    price = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.CLASS_NAME, "_2v0Hgx"))
-    print(price.text)
+for option in options:
+    if option.text not in default_options:
+        available_options.append(option.text)
+print(available_options)
 
-    quantity = driver.find_element(By.CSS_SELECTOR, "div.flex.items-center.L6Jueq")
-    print(quantity.text)
+"""Create dict to store the options and variations"""
+variation_dict = {}
+variations = driver.find_elements(by=By.CLASS_NAME, value='_3ABAc7')
+for i in range(len(variations)):
+    variation_dict[available_options[i]] = variations[i].text.split()
+print(variation_dict)
 
-# """
-# Search for not available variations
-# """
+# available_variations = WebDriverWait(driver, timeout=5).until(
+#     lambda d: d.find_elements(By.XPATH, '//button[@class="product-variation" and @aria-disabled="false"]'))
+# for variation in available_variations:
+#     print(variation.text)
+#     variation.click()
+#     sleep(1)
+#
+#     price = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.CLASS_NAME, "_2v0Hgx"))
+#     print(price.text)
+#
+#     quantity = driver.find_element(By.CSS_SELECTOR, "div.flex.items-center.L6Jueq")
+#     print(quantity.text)
+
+"""
+Search for not available variations
+"""
 # not_available_variations = WebDriverWait(driver, timeout=10).until(
 #     lambda d: d.find_elements(By.XPATH, '//button[@class="product-variation.product-variation--disabled" and '
 #                                         '@aria-disabled="true"]'))
@@ -58,5 +73,5 @@ for variation in available_variations:
 """
 Search shop name
 """
-shop_name = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.CLASS_NAME, "_1wVLAc"))
-print(shop_name.text)
+# shop_name = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.CLASS_NAME, "_1wVLAc"))
+# print(shop_name.text)
