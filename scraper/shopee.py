17f2dd780caea7558a636711a741161dd7467903
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2022-03-30 06:15:26 +0700

    Query another field, make function

--- scraper/shopee.py
+++ scraper/shopee.py
@@ -8,32 +8,112 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
-# driver.get("https://shopee.co.th/Apple-iPhone-13-%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2%E0%B8%88%E0%B8%AD-6.1-%E0%B8%99"
-#            "%E0%B8%B4%E0%B9%89%E0%B8%A7-i.287137993.10951724608?sp_atk=f674b50f-08d3-47a2-b7da-ac0606b4f4f7")
-driver.get("https://shopee.co.th/%F0%9F%9A%9A%E0%B8%AA%E0%B9%88%E0%B8%87%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B8%97%E0%B8%B8"
-           "%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%99-%E0%B8%81%E0%B8%B2%E0%B8%87%E0%B9%80%E0%B8%81%E0%B8%87%E0%B8%A7%E0"
-           "%B8%B4%E0%B8%99%E0%B9%80%E0%B8%97%E0%B8%88%E0%B8%97%E0%B8%A3%E0%B8%87%E0%B8%8A%E0%B9%88%E0%B8%B2%E0%B8%87"
-           "-%E0%B9%80%E0%B8%AD%E0%B8%A726-32-%E0%B8%A1%E0%B8%B5%E0%B9%84%E0%B8%8B%E0%B8%95%E0%B9%8C%E0%B8%88%E0%B8"
-           "%B1%E0%B8%A1%E0%B9%82%E0%B8%9A%E0%B9%89%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2%E0%B8%99%E0%B8%B0%E0%B8%84%E0"
-           "%B8%B0-%E0%B9%80%E0%B8%AA%E0%B8%B7%E0%B9%89%E0%B8%AD%E0%B8%9C%E0%B9%89%E0%B8%B2%E0%B9%81%E0%B8%9F%E0%B8"
-           "%8A%E0%B8%B1%E0%B9%88%E0%B8%99-%E0%B8%AA%E0%B8%B8%E0%B8%94%E0%B8%AE%E0%B8%B4%E0%B8%95-i.151206650"
-           ".4157186287?sp_atk=2215c8ac-cd93-40d7-9970-fa30f2308c9a&xptdk=2215c8ac-cd93-40d7-9970-fa30f2308c9a")
+driver.get("https://shopee.co.th/Apple-iPhone12-%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2%E0%B8%88%E0%B8%AD-6.1-%E0%B8%99"
+           "%E0%B8%B4%E0%B9%89%E0%B8%A7-i.287137993.7862109494?sp_atk=cbb0583f-e97e-4e1e-b9bb-e6ccd583927c&xptdk"
+           "=cbb0583f-e97e-4e1e-b9bb-e6ccd583927c")
+# driver.get("https://shopee.co.th/Apple-Watch-Series-7-GPS-("
+#            "2021)-%E0%B8%AA%E0%B8%B2%E0%B8%A2-Sport-Band-i.287137993.11167901407")
+# driver.get("https://shopee.co.th/%E0%B8%9B%E0%B8%B2%E0%B8%81%E0%B8%81%E0%B8%B2%E0%B8%84%E0%B8%A7%E0%B8%AD%E0%B8%99%E0"
+#            "%B8%95%E0%B8%B1%E0%B9%89%E0%B8%A1-Marshmallow-0.29-%E0%B8%A1%E0%B8%A1.-%E0%B8%AB%E0%B8%A1%E0%B8%B6%E0%B8"
+#            "%81%E0%B8%AA%E0%B8%B5%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B9%80%E0%B8%87%E0%B8%B4%E0%B8%99-("
+#            "1%E0%B8%94%E0%B9%89%E0%B8%B2%E0%B8%A1)-i.87642797.5502488788?sp_atk=0fda7e23-1cf6-429e-b80b-95c28c20d59e"
+#            "&xptdk=0fda7e23-1cf6-429e-b80b-95c28c20d59e")
 print("Load Shopee item successful.")
 
-"""
-Click Thai language selected at welcome.
-"""
+
+def query_price(obj):
+    price_div = obj.find_element(By.CLASS_NAME, "_2v0Hgx")
+    price = int(''.join(filter(str.isdigit, price_div.text)))
+    return price
+
+
+def query_product_image(obj):
+    image_div = obj.find_element(By.CLASS_NAME, "_3rslob")
+    style = image_div.get_attribute("style")
+    text = style.split('"', 1)[1].split('"', 1)[0]
+    return text
+
+
+def query_rating_score(obj):
+    span = obj.find_element(By.CLASS_NAME, 'product-rating-overview__rating-score')
+    return span.text
+
+
+def query_rating_voter(obj):
+    div_text = obj.find_elements(By.CLASS_NAME, '_3uBhVI')[1].text
+    if "พัน" in div_text:
+        rating_voter = int(float(div_text.replace("พัน", "")) * 1000)
+    else:
+        rating_voter = int(div_text)
+    return rating_voter
+
+
+def query_product_sold(obj):
+    div_text = obj.find_element(By.CLASS_NAME, '_3b2Btx').text
+    if "พัน" in div_text:
+        product_sold = int(float(div_text.replace("พัน", "")) * 1000)
+    else:
+        product_sold = int(div_text)
+    return product_sold
+
+
+def query_store_name(obj):
+    div = obj.find_element(By.CLASS_NAME, '_1wVLAc')
+    return div.text
+
+
+def query_store_link(obj):
+    a = obj.find_element(By.CLASS_NAME, '_3IIjTV')
+    store_link = a.get_attribute('href')
+    return store_link
+
+
+def query_store_avatar(obj):
+    div = obj.find_element(By.CLASS_NAME, '_2T4rHi')
+    img = div.find_element(By.CLASS_NAME, 'shopee-avatar__img')
+    img_link = img.get_attribute('src')
+    return img_link
+
+
+"""Click Thai language selected at welcome."""
 print("Start to select language dialog.")
 language_dialog = WebDriverWait(driver, timeout=5).until(
     lambda d: d.find_element(By.CLASS_NAME, "shopee-button-outline.shopee-button-outline--primary-reverse "))
 language_dialog.click()
 print("Click language dialog successful.")
 
-"""
-Collect product name.
-"""
+"""Collect product name."""
 product_name = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.CSS_SELECTOR, "div._3g8My- > span"))
 print(product_name.text)
+
+"""Collect product image."""
+image = query_product_image(driver)
+print(image)
+
+"""Collect store name."""
+store_name = query_store_name(driver)
+print(store_name)
+
+"""Collect store link."""
+store_link = query_store_link(driver)
+print(store_link)
+
+"""Collect store avatar."""
+store_avatar = query_store_avatar(driver)
+print(store_avatar)
+
+"""Collect rating score."""
+rating_score = query_rating_score(driver)
+print(rating_score)
+
+"""Collect rating voter."""
+rating_voter = query_rating_voter(driver)
+print(rating_voter)
+
+"""Collect product sold."""
+product_sold = query_product_sold(driver)
+print(product_sold)
+
 
 """
 Search for available variations.
@@ -51,6 +131,7 @@ print(available_options)
 """Create dict to store the options and variations"""
 variation_dict = {}
 variations = driver.find_elements(by=By.CLASS_NAME, value='_3ABAc7')
+
 
 """Check number of variations then scraping."""
 if len(available_options) == 2:  # If the product has 2 variations.
@@ -72,12 +153,14 @@ if len(available_options) == 2:  # If the product has 2 variations.
                         if select2.text == button2.text:
                             quantity_div = driver.find_element(by=By.CLASS_NAME, value='L6Jueq')
                             quantity = int(''.join(filter(str.isdigit, quantity_div.text)))
-                            print(button1.text, button2.text, quantity)
+                            price = query_price(driver)
+                            print(button1.text, button2.text, quantity, price)
                     except Exception:
-                        print("Button 2 unavailable")
+                        print(button1.text, button2.text, 0, None)
                     button2.click()
         except Exception:
-            print("Button 1 unavailable")
+            for button2 in variation2.find_elements(by=By.TAG_NAME, value='button'):
+                print(button1.text, button2.text, 0, None)
 elif len(available_options) == 1:  # If the product has only variation.
     variation = variations[0]
     for button in variation.find_elements(by=By.TAG_NAME, value='button'):
@@ -88,35 +171,15 @@ elif len(available_options) == 1:  # If the product has only variation.
             if select.text == button.text:
                 quantity_div = driver.find_element(by=By.CLASS_NAME, value='L6Jueq')
                 quantity = int(''.join(filter(str.isdigit, quantity_div.text)))
-                print(button.text, quantity)
+                price = query_price(driver)
+                print(button.text, quantity, price)
             else:
-                print(button.text, 0)
+                print(button.text, 0, None)
         except Exception:
             print("Button unavailable")
+        button.click()
 # print(variation_dict)
 
-# available_variations = WebDriverWait(driver, timeout=5).until(
-#     lambda d: d.find_elements(By.XPATH, '//button[@class="product-variation" and @aria-disabled="false"]'))
-# for variation in available_variations:
-#     print(variation.text)
-#     variation.click()
-#     sleep(1)
-#
-#     price = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.CLASS_NAME, "_2v0Hgx"))
-#     print(price.text)
-#
-#     quantity = driver.find_element(By.CSS_SELECTOR, "div.flex.items-center.L6Jueq")
-#     print(quantity.text)
-
-"""
-Search for not available variations
-"""
-# not_available_variations = WebDriverWait(driver, timeout=10).until(
-#     lambda d: d.find_elements(By.XPATH, '//button[@class="product-variation.product-variation--disabled" and '
-#                                         '@aria-disabled="true"]'))
-# for variation in not_available_variations:
-#     print(variation.text)
-#     print("price = 0, available = 0")
 
 """
 Search shop name
