e67c5b82b8ba8dc0c0c61bee6c792e14ab758120
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2022-04-16 03:32:18 +0700

    Start

--- website/models.py
+++ website/models.py
@@ -1,3 +1,24 @@
+import uuid as uuid
 from django.db import models
 
-# Create your models here.
+
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Store(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    link = models.URLField()
+    avatar = models.URLField()
+
+
+class ShopeeItem(TimeStampMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField()
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
